9d89e4cf553f51686bfbf501aaf7fdf34ac8841a
dudals2467 <dudals2467@kookmin.ac.kr>
2022-02-13 17:53:57 +0900

    0213

--- 그래프/아기상어.py
+++ 그래프/아기상어.py
@@ -1,68 +1,96 @@
+# https://www.acmicpc.net/problem/16236 삼성 기출문제
 from collections import deque
+import copy
 
-dxs = [-1, 0, 0, 1]
-dys = [0, -1, 1, 0]
+N = int(input())
 
-def bfs(x, y):
-    q, visited = deque([(x, y)]), set([(x, y)])
-    time = 0
-    shark = 2  # 현재 아기 상어의 크기다.
-    eat = 0    # 현재 크기에서, 지금까지 먹은 물고기 수다.
-    eat_flag = False  # 현재 상태에서 물고기를 먹은 경우, 
-                      # for _ in range(size) 구문을 진행하지 않기 위한 플래그다.
-    answer = 0
+arr = []
+size = [2,0]
 
-    while q:
-        size = len(q)
+for i in range(N):
+    arr.append(list(map(int, input().split())))
 
-        # 위, 그리고 왼쪽을 더 우선시해서 가야하기 때문에, BFS queue를 소팅해준다.
-        q = deque(sorted(q))
-        for _ in range(size):
-            x, y = q.popleft()
+cnt_where = []
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] == 9:
+            cnt_where.append(i)
+            cnt_where.append(j)
 
-            # 현재 위치에 아기 상어보다 작은 물고기가 있어서, 이를 먹은 경우.
-            if board[x][y] != 0 and board[x][y] < shark:
-                board[x][y] = 0
-                eat += 1
+arr[cnt_where[0]][cnt_where[1]] = 0
 
-                # 아기 상어의 크기 만큼 먹었다면, 아기 상어의 크기를 +1 해줘야한다.
-                if eat == shark:
-                    shark += 1
-                    eat = 0               
+def bfs():
+    arr_copy = copy.deepcopy(arr)
 
-                # 먹고 난 뒤, 현재 위치를 기준으로 다시 근처를 탐색해야 하기 때문에,
-                # BFS queue 와 visited 를 초기화 해준다.
-                q, visited = deque(), set([(x, y)])
-                eat_flag = True
+    for i in range(N):
+        for j in range(N):
+            if arr_copy[i][j] > size[0] and arr_copy[i][j] != 9:
+                arr_copy[i][j] = 100
+            else:
+                arr_copy[i][j] = 0
+    
 
-                # 먹었을 때의 시간을 저장해둔다.
-                answer = time
+    queue = deque([[cnt_where[0], cnt_where[1]]])
 
-            for dx, dy in zip(dxs, dys):
-                nx, ny = x + dx, y + dy
-                if nx >= 0 and nx < n and ny >= 0 and ny < n and (nx, ny) not in visited:
-                    if board[nx][ny] <= shark:
-                        q.append((nx, ny))
-                        visited.add((nx, ny))
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
+    
+    while queue:
+        X, Y = queue.popleft()
+        
+        for i in range(4):
+            real_X = X + dx[i]
+            real_Y = Y + dy[i]
 
-            # 현재 위치에서 먹었다면, 더 이상 위 반복문을 돌 필요가 없다.
-            if eat_flag:
-                eat_flag = False
-                break
+            if real_X < 0 or real_Y < 0 or real_X >= N or real_Y >= N or arr_copy[real_X][real_Y] == 100:
+                continue  
+            if arr_copy[real_X][real_Y] != 0 or (real_X == cnt_where[0] and real_Y == cnt_where[1]):
+                continue
+            arr_copy[real_X][real_Y] = arr_copy[X][Y] + 1
+            queue.append([real_X,real_Y])
 
-        time += 1
-    return answer
+    return arr_copy
 
-n = int(input())
-board = [list(map(int , input().split())) for _ in range(n)]
+def find_to_eat():
+    find_eat = []
+    for i in range(N):
+        for j in range(N):
+            if arr[i][j] < size[0] and arr[i][j] != 0:
+                find_eat.append([i,j])
+    
+    return find_eat
 
-# 1. 초기 상어(자신)의 위치를 파악하고, 해당 자리는 판에서 비워둔다.
-s_x, s_y = None, None
-for i in range(n):
-    for j in range(n):
-        if board[i][j] == 9:
-            s_x, s_y = i, j
-            board[i][j] = 0
+cnt_value = 0
+cnt = 0
+while True:
+    can_eat = find_to_eat() # [0,3], [3,0]
+    if len(can_eat) == 0:
+        break
+    bfs_result = bfs()
+    result = []
+    
+    cnt += 1
+    
+    for i in can_eat:
+        result.append([bfs_result[i[0]][i[1]], i[0], i[1]])
+    
+    result = sorted(result, key = lambda X : (X[0], X[1], X[2]))
+    while result:
+        if result[0][0] == 0:
+            result.pop(0)
+        else:
+            break
+    if len(result) == 0:
+        break
 
-# 2. 시작점에서 BFS 진행
-print(bfs(s_x, s_y))
+    cnt_value += result[0][0]
+    size[1] += 1
+    if size[0] == size[1]:
+        size[0] += 1
+        size[1] = 0
+
+    arr[result[0][1]][result[0][2]] = 0
+
+    cnt_where = [result[0][1], result[0][2]]
+    
+print(cnt_value)
